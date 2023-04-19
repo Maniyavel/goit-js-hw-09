de263e380093fe603a3885e5b1e4a6f1b675bac0
Maniyavel <116820894+Maniyavel@users.noreply.github.com>
2023-04-19 21:08:57 +0300

    fixed t1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,19 +1,19 @@
 const refs={
     startButton:document.querySelector('[data-start]'),
-    stopButton:document.querySelector('data-stop'),
-    body:document.querySelector('body'),
+    stopButton:document.querySelector('[data-stop]'),
+    body:document.querySelector('body')
 }
  
-let intervalId=null;
+let interval=null;
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215)
       .toString(16)
       .padStart(6, 0)}`;
-  };
+  }
 
   refs.startButton.addEventListener('click', onColor);
-  refs.stopButton.addEventListener('click', ofColor);
+  refs.stopButton.addEventListener('click', offColor);
 
 const onColor=()=>{
 const intervalId=setInterval(()=>{
@@ -23,8 +23,8 @@ startButton.setAttribute('disabled', 'disabled');
 stopButton.removeAttribute('disabled');
 }
 
-const ofColor=()=>{
+const offColor=()=>{
     startButton.removeAttribute('disabled');
     stopButton.setAttribute('disabled', 'disabled');
-    clearInterval(intervalId);
+    clearInterval(interval);
 };
