8f343438dc77a2878e079e2a9832cb81e1542b2a
Maniyavel <116820894+Maniyavel@users.noreply.github.com>
2023-04-24 12:20:33 +0300

    add task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const form=document.querySelector('.form');
+form.addEventListener('submit', onsubmit );
+
+function onsubmit(evt){
+  evt.preventDefault();
+  let.amount=form.amount.value;
+  let.delay=form.delay.value;
+  delay=Number(delay);
+  let.step=form.step.value;
+  step=Number(step);
+ 
+
+  for(i=1; i<=amount; i++){
+createPromise(i, delay)
+.then(value=>console.loc(value))
+.catch(err=>console.log(err));
+delay += step;
   }
 }
+
+
+
+function createPromise(position, delay) {
+  return new Promise((res, rej)=>{
+    const shouldResolve = Math.random() > 0.3;
+  setTimeout(()=>{
+    if (shouldResolve) {
+    res(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  } else {
+     rej(`❌ Rejected promise ${position} in ${delay}ms`);
+  }}, delay);
+  });
+}
+
+
+
+
