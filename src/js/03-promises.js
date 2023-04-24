1cb24ed4e58952386e67ed4857a5270317a36263
Maniyavel <116820894+Maniyavel@users.noreply.github.com>
2023-04-24 12:54:28 +0300

    updated task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,36 +1,41 @@
 const form=document.querySelector('.form');
-form.addEventListener('submit', onsubmit );
+form.addEventListener('submit', onSubmit );
 
-function onsubmit(evt){
+function createPromise(position, delay) {  
+  return new Promise((res, rej)=>{
+  const shouldResolve = Math.random() > 0.3;
+setTimeout(()=>{
+  if (shouldResolve) {
+  res({position, delay});
+} else {
+   rej({position, delay});
+}}, delay);
+});}
+
+
+function onSubmit(evt){
   evt.preventDefault();
-  let amount=form.amount.value;
-  let delay=form.delay.value;
-  delay=Number(delay);
-  let step=form.step.value;
-  step=Number(step);
+  const{delay, step, amount}=evt.currentTarget.elements;
  
 
-  for(i=1; i<=amount; i++){
-createPromise(i, delay)
-.then(value=>console.loc(value))
-.catch(err=>console.log(err));
-delay += step;
-  }
-}
+  for(let i=0, position=1; i<=amount.value; i+=1, position+=1){
+const delayAll=Number(delay.value)+step.value*i;
 
-
-
-function createPromise(position, delay) {
-  return new Promise((res, rej)=>{
-    const shouldResolve = Math.random() > 0.3;
-  setTimeout(()=>{
-    if (shouldResolve) {
-    res(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  } else {
-     rej(`❌ Rejected promise ${position} in ${delay}ms`);
-  }}, delay);
+createPromise(position, delayAll)
+  .then(({ position, delay }) => {
+    return(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    return (`❌ Rejected promise ${position} in ${delay}ms`);
   });
 }
+
+}
+
+ 
+
+
+
 
 
 
